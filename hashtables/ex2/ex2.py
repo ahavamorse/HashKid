4b8aa0601d67b5265e848757c4a1a2441dfc6a9b
ahavamorse <ahavamorse@icloud.com>
2021-06-02 09:00:17 -0700

    Implemented exercise 2 - reconstruct trip using a hashtable

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -9,6 +9,24 @@ def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    dict = {}
+    starting_airport = None
+    ending_airport = None
+
+    for ticket in tickets:
+        if ticket.source != "NONE":
+            if ticket.destination != "NONE":
+                dict[ticket.source] = ticket.destination
+            else:
+                ending_airport = ticket.source
+        else:
+            starting_airport = ticket.destination
+
+    route = ["NONE"] * length
+    current_airport = starting_airport
+    for i in range(0, length - 1):
+        route[i] = current_airport
+        if current_airport != ending_airport:
+            current_airport = dict[current_airport]
 
     return route
